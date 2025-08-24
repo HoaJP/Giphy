0230c08a8bb2e8273f894b0ae4506ed710ee5b2d
Hoa JP <itlmh2k3@gmail.com>
2025-08-24 15:26:43 +0700

    first edition

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Search from "./pages/Search";
+import SingleGif from "./pages/SingleGif";
+import Favorites from "./pages/Favorites";
+import GifProvider from "./context/GifContext";
+
+//homepage
+//categories
+//search
+//single gif
+//favorites
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+
+    children: [
+      {
+        path: "/",
+
+        element: <Home />,
+      },
+      {
+        path: "/:category",
+
+        element: <Category />,
+      },
+      {
+        path: "/search/:query",
+
+        element: <Search />,
+      },
+      {
+        path: "/:type/:slug",
+
+        element: <SingleGif />,
+      },
+      {
+        path: "/favorites",
+
+        element: <Favorites />,
+      },
+    ],
+  },
+]);
+function App() {
+  return (
+    <GifProvider>
+      <RouterProvider router={router} />
+    </GifProvider>
+  );
+}
+
+export default App;
